dc0512289b83e5e359bcb30174bfc8ae3d5cd7ae
marcelosf89 <marcelo_vp@oi.com.br>
2013-09-24 01:26:39 -0300

    Inclusão do @Ajax.Bootstrap().ActionLinkButton() no TwitterBootstrapMVC

---
+++ src/TwitterBootstrapMVC/TwitterBootstrapMVC/BootstrapMethods/AjaxBootstrap.ActionLinkButton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using TwitterBootstrapMVC.Controls;
+
+namespace TwitterBootstrapMVC.BootstrapMethods
+{
+    public partial class AjaxBootstrap<TModel>
+    {
+        public AjaxBootstrapActionLinkButton ActionLinkButton(string linkText, AjaxOptions result)
+        {
+            return new AjaxBootstrapActionLinkButton(Ajax, linkText, result);
+        }
+
+        public AjaxBootstrapActionLinkButton ActionLinkButton(string linkText, string actionName)
+        {
+            return new AjaxBootstrapActionLinkButton(Ajax, linkText, actionName);
+        }
+
+        public AjaxBootstrapActionLinkButton ActionLinkButton(string linkText, string actionName, string controllerName)
+        {
+            return new AjaxBootstrapActionLinkButton(Ajax, linkText, actionName, controllerName);
+        }
+
+        public AjaxBootstrapActionLinkButton ActionLinkButton(string linkText, string actionName, string controllerName, AjaxOptions result)
+        {
+            return new AjaxBootstrapActionLinkButton(Ajax, linkText, actionName, controllerName, result);
+        }
+    }
+}
