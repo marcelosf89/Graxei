c92c3d833b54a197e66eeb4a7b636f0144d15034
marcelosf89 <masf89.rj@gmail.com>
2014-10-28 14:45:11 -0200

    Ajuste na autenticação Geral (local e google)

--- src/Graxei/Graxei.Apresentacao.MVC4Unity/Controllers/LoginController.cs
+++ src/Graxei/Graxei.Apresentacao.MVC4Unity/Controllers/LoginController.cs
@@ -58,16 +58,14 @@ namespace Graxei.Apresentacao.MVC4Unity.Controllers
 
             if (OAuthWebSecurity.Login(result.Provider, result.ProviderUserId, createPersistentCookie: false))
             {
-                return Json(new { url = Url.Action("Index", "Home") });
-                //return RedirectToLocal(returnUrl);
+                return RedirectToAction("Index", "Home");
             }
 
             if (User.Identity.IsAuthenticated)
             {
                 // If the current user is logged in add the new account
                 OAuthWebSecurity.CreateOrUpdateAccount(result.Provider, result.ProviderUserId, User.Identity.Name);
-                //return RedirectToLocal(returnUrl);
-                return Json(new { url = Url.Action("Index", "Home") });
+                return RedirectToAction("Index", "Home");
             }
             else
             {
@@ -75,8 +73,7 @@ namespace Graxei.Apresentacao.MVC4Unity.Controllers
                 string loginData = OAuthWebSecurity.SerializeProviderUserId(result.Provider, result.ProviderUserId);
                 ViewBag.ProviderDisplayName = OAuthWebSecurity.GetOAuthClientData(result.Provider).DisplayName;
                 ViewBag.ReturnUrl = returnUrl;
-                //return View("ExternalLoginConfirmation", new RegisterExternalLoginModel { UserName = result.UserName, ExternalLoginData = loginData });
-                return Json(new { url = Url.Action("Index", "Home") });
+                return RedirectToAction("Index", "Home");
             }
         }
 
