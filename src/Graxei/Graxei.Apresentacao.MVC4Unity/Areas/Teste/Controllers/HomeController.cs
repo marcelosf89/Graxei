ecbc90862aad83a8ce90cbdf671c19fe09b8b0bc
marcelosf89 <masf89.rj@gmail.com>
2014-11-14 12:51:16 -0200

    Inclusaõ da pagina de teste

---
+++ src/Graxei/Graxei.Apresentacao.MVC4Unity/Areas/Teste/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿
+using Graxei.Apresentacao.MVC4Unity.Areas.Teste.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Graxei.Apresentacao.MVC4Unity.Areas.Teste.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Teste/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CarregarPagina(String txtRota)
+        {
+            String[] obj = txtRota.Split(new char[] { '?' }, StringSplitOptions.RemoveEmptyEntries);
+            String[] vals = obj[0].Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            
+            
+            ObjectTesteModel obm = new ObjectTesteModel();
+            obm.Area = vals.Length > 2 ? vals[0]: "";
+            obm.Controller = vals.Length > 2 ? vals[1] : vals[0];
+            obm.View = vals.Length > 2 ? vals[2] : vals[1]; ;
+
+            dynamic expando = new ExpandoObject();
+            IDictionary<String, object> dic = expando as IDictionary<String, object>;
+
+            dic["Controller"] = obm.Controller;
+            dic["Area"] = obm.Area;
+
+            //if (obj.Length > 1)
+            //{
+            //    vals = obj[1].Split(new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+            //    foreach (String item in vals)
+            //    {
+            //        string[] splitValor = item.Split('=');
+            //        dic[splitValor[0]] = Convert.ToInt32(splitValor[1]);
+            //    }
+            //}
+
+            obm.Route = new { Controller = expando.Controller, Area = expando.Area }; ;
+            return View(obm);
+        }
+        
+
+    }
+}
---
+++ src/Graxei/Graxei.Apresentacao.MVC4Unity/Areas/Teste/Models/ObjectTesteModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Graxei.Apresentacao.MVC4Unity.Areas.Teste.Models
+{
+    public class ObjectTesteModel
+    {
+        public String View { get; set; }
+
+        public string Area { get; set; }
+
+        public string Controller { get; set; }
+
+        public object Route { get; set; }
+    }
+}
