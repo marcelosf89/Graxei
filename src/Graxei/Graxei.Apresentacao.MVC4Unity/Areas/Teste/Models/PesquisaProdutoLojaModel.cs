6819d6f46ec537536dc94e9095d018e86fb95709
Everton Novaes <ebnovaes@gmail.com>
2015-01-14 17:24:23 -0200

    - Consertando simplesmente a PESQUISA PRINCIPAL!!!!
    - Refatorando a pesquisa SEM SER A PRINCIPAL!!!

--- src/Graxei/Graxei.Apresentacao.MVC4Unity/Areas/Teste/Models/PesquisaProdutoLojaModel.cs
+++ src/Graxei/Graxei.Apresentacao.MVC4Unity/Areas/Teste/Models/PesquisaProdutoLojaModel.cs
@@ -8,46 +8,14 @@ namespace Graxei.Apresentacao.MVC4Unity.Areas.Teste.Models
 {
     public class PesquisaProdutoLojaModel
     {
-        private TotalElementosLista _totalElementosLista;
-
-        private PaginaAtualLista _paginaAtualLista;
-
         public long IdLoja { get; set; }
         
         public string DescricaoProduto { get; set; }
         
         public bool MeusProdutos { get; set; }
 
-        public PaginaAtualLista PaginaAtualLista
-        {
-            get
-            {
-                if (_paginaAtualLista == null)
-                {
-                    _paginaAtualLista = new PaginaAtualLista(1);
-                }
-                return _paginaAtualLista;
-            }
-            set
-            {
-                _paginaAtualLista = value;
-            }
-        }
+        public long PaginaAtualLista { get; set; }
 
-        public TotalElementosLista TotalElementosLista
-        {
-            get
-            {
-                if (_totalElementosLista == null)
-                {
-                    _totalElementosLista = new TotalElementosLista(1);
-                }
-                return _totalElementosLista;
-            }
-            set
-            {
-                _totalElementosLista = value;
-            }
-        }
+        public long TotalElementosLista { get; set; }
     }
 }
--- src/Graxei/Graxei.Apresentacao.MVC4Unity/Extension/MVCExtensionComponentPaginacao.cs
+++ src/Graxei/Graxei.Apresentacao.MVC4Unity/Extension/MVCExtensionComponentPaginacao.cs
@@ -13,7 +13,74 @@ namespace Graxei.Apresentacao.MVC4Unity.Extension
 {
     public static partial class MVCExtensionComponent
     {
+        public static MvcHtmlString LinkPaginacaoRangePagina(this AjaxHelper ajaxHelper, string action, string controller, string requestName, int paginaSelecionada, long paginasMaxima, int quantidadePaginasAbaixoAtual)
+        {
+            return LinkPaginacaoRangePagina(ajaxHelper, action, controller, requestName, paginaSelecionada, paginasMaxima, quantidadePaginasAbaixoAtual, "myContent");
+        }
+
+        public static MvcHtmlString LinkPaginacaoRangePagina(this AjaxHelper ajaxHelper, string action, string controller, string requestName, int paginaSelecionada, long paginasMaxima, int quantidadePaginasAbaixoAtual, String updateTargetId)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            long idx = ((paginaSelecionada <= quantidadePaginasAbaixoAtual + 1) ? 0 : paginaSelecionada - quantidadePaginasAbaixoAtual);
+
+            System.Web.Mvc.Ajax.AjaxOptions ao = new System.Web.Mvc.Ajax.AjaxOptions();
+            ao.OnBegin = "openL()"; ao.OnComplete = "closeL()"; ao.UpdateTargetId = updateTargetId;
+            ao.HttpMethod = "GET";
+
+            sb.Append("<div class=\"btn-group\">");
+            if (paginaSelecionada > 0)
+            {
+                RouteValueDictionary rd = new RouteValueDictionary();
+                rd.Add("Controller", controller);
+                rd.Add(requestName, (paginaSelecionada - 1));
+
+                sb.Append(
+                    ajaxHelper.IconActionLink("glyphicon glyphicon-chevron-left", "", action, controller, rd, ao, new Dictionary<string, object> { { "class", "btn btn-default" } }).ToHtmlString()
+                    );
+            }
+
+            for (long i = idx; i <= paginasMaxima; i++)
+            {
+                RouteValueDictionary rd = new RouteValueDictionary();
+                rd.Add("Controller", controller);
+                rd.Add(requestName, i);
+
+                if (paginaSelecionada == i)
+                {
+                    sb.Append(ajaxHelper.IconActionLink("", (i + 1).ToString(), "#", "#", rd, ao, new Dictionary<string, object> { { "class", "btn btn-warning" }, { "disabled", "disabled" } }).ToHtmlString());
+                }
+                else
+                {
+                    sb.Append(ajaxHelper.IconActionLink("", (i + 1).ToString(), action, controller, rd, ao, new Dictionary<string, object> { { "class", "btn btn-default" } }).ToHtmlString());
+                }
+            }
+            if (paginasMaxima > 0 && paginaSelecionada < paginasMaxima)
+            {
+                RouteValueDictionary rd = new RouteValueDictionary();
+                rd.Add("Controller", controller);
+                rd.Add(requestName, (paginaSelecionada + 1));
+
+                sb.Append(
+                    ajaxHelper.IconActionLink("glyphicon glyphicon-chevron-right", "", action, controller, rd, ao, new Dictionary<string, object> { { "class", "btn btn-default" } }).ToHtmlString()
+                );
+            }
+            sb.Append("</div>");
+            return new MvcHtmlString(sb.ToString());
+        }
+
         public static MvcHtmlString LinkPaginacao(this AjaxHelper ajaxHelper, string controller, string action, PaginaAtualLista paginaAtualLista, TotalElementosLista listaTotalElementos, int maximoElementosPaginacao)
+        {
+            if (listaTotalElementos.Total <= 0)
+            {
+                return MvcHtmlString.Create("<div>Nenhum resultado</div>");
+            }
+            LinkBuilderPadraoStrategy linkBuilder = new LinkBuilderPadraoStrategy(ajaxHelper, controller, action, paginaAtualLista);
+            PaginacaoChainFactory paginacaoChainFactory = new PaginacaoChainFactory(ajaxHelper, listaTotalElementos, paginaAtualLista, maximoElementosPaginacao, controller, action, linkBuilder);
+            return paginacaoChainFactory.ConstruirCadeiaDePaginacao().Get();
+        }
+
+        public static MvcHtmlString LinkPaginacaoPesquisaProdutos(this AjaxHelper ajaxHelper, string controller, string action, PaginaAtualLista paginaAtualLista, TotalElementosLista listaTotalElementos, int maximoElementosPaginacao)
         {
             if (listaTotalElementos.Total <= 0)
             {
