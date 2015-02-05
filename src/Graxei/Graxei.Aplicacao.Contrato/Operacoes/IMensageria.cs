e462eebef1337ab672f891f9b7657b1d4e1aa523
Gilliard <gilliard.costa@teewe.com.br>
2015-02-05 14:44:43 -0200

    Alteração no tratamento de e-mail.

---
+++ src/Graxei/Graxei.Aplicacao.Contrato/Operacoes/IMensageria.cs
@@ -0,0 +1,9 @@
+﻿
+using Graxei.Modelo;
+namespace Graxei.Aplicacao.Contrato.Operacoes
+{
+    public interface IGerenciamentoMensageria
+    {
+        void Enviar(Mensagem mensagem, ConfiguracaoMail configuracao);
+    }
+}
---
+++ src/Graxei/Graxei.Modelo/ConfiguracaoMail.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.CompilerServices;
+using FAST.Modelo;
+using System;
+using System.Collections.Generic;
+
+namespace Graxei.Modelo
+{
+    public class ConfiguracaoMail 
+    {
+        public ConfiguracaoMail(string credencialNome, string credencialSenha, string servidor, int porta, string remetenteNome, string remetendeEndereco)
+        {
+            this.CredencialNome = credencialNome;
+            this.CredencialSenha = credencialSenha;
+            this.Servidor = servidor;
+            this.Porta = porta;
+            this.RemetenteNome = remetenteNome;
+            this.RemetenteEndereco = remetendeEndereco;
+        }
+                    
+        public virtual string CredencialNome { get; set; }
+
+        public virtual string CredencialSenha { get; set; }
+
+        public virtual string Servidor { get; set; }
+        
+        public virtual int Porta { get;  set; }
+
+        public virtual string RemetenteNome { get; set; }
+
+        public virtual string RemetenteEndereco { get; set; }
+
+        #region Métodos Sobrescritos
+        public override bool Equals(object obj)
+        {
+            if (!(obj is ConfiguracaoMail))
+            {
+                return false;
+            }
+            ConfiguracaoMail cfm = (ConfiguracaoMail)obj;
+            return (cfm.Servidor == this.Servidor && cfm.Porta == this.Porta);
+        }
+
+        public override int GetHashCode()
+        {
+            int retorno = 0;
+            if (!(String.IsNullOrEmpty(Servidor)))
+            {
+                retorno += Servidor.GetHashCode();
+            }
+            return retorno;
+        }
+
+        #endregion
+
+    }
+}
