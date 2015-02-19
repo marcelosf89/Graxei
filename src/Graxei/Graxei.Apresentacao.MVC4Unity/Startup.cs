3a9806864c61b24fba77187d800fc9c42d8d7bf8
marcelosf89 <masf89.rj@gmail.com>
2015-02-19 11:55:36 -0200

    Inclusão do SignalR

---
+++ src/Graxei/Graxei.Apresentacao.MVC4Unity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(Graxei.Apresentacao.MVC4Unity.Startup))]
+namespace Graxei.Apresentacao.MVC4Unity
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
