9ade93a4717aacef79dcda7931b4910267bf0837
Everton Novaes <ebnovaes@gmail.com>
2016-01-08 15:41:13 -0200

    Criando comportamentos dos botões

--- src/Graxei/Graxei.Apresentacao/Scripts/Graxei/Administrativo/Produtos/pesquisar-produtos.js
+++ src/Graxei/Graxei.Apresentacao/Scripts/Graxei/Administrativo/Produtos/pesquisar-produtos.js
@@ -7,6 +7,10 @@
         var controller = this;
 
         this.produtos = {};
+
+        this.exibirBotoes = false;
+
+        this.listaOriginal = {};
         //    lista: [{
         //        id: 111,
         //        codigo: '999',
@@ -22,36 +26,80 @@
         //};
 
         this.init = function (pesquisaModel) {
-            debugger;
-            $http.post("/Administrativo/Produtos/PesquisarJson", { params: { model: pesquisaModel } })
-                   .then(function (response) {
-
-                       controller.produtos = response.data;
-                   });
-
+            controller.postData(pesquisaModel, function (response) {
+                controller.produtos = response;
+                controller.listaOriginal = controller.produtos;
+            });
         }
 
-        this.listaOriginal = this.produtos.lista;
+        this.postData = function (pesquisaModel, funcaoCallback) {
+            $http.post("/Administrativo/Produtos/PesquisarJson", { model: pesquisaModel })
+                  .then(function (response) {
+                      funcaoCallback(response.data);
+                  });
+        }
 
         this.habilitarEdicao = function (itemLista, exibir) {
-            debugger;
             if (exibir === undefined) {
                 exibir = true;
+            }
+            if (itemLista.minhaDescricao === null || itemLista.minhaDescricao.length === 0) {
+                itemLista.minhaDescricao = itemLista.descricaoOriginal;
             }
             itemLista.exibir = exibir;
         };
 
         this.desfazerEdicao = function (itemLista) {
-            var listaOriginal = controller.listaOriginal;
+            var listaOriginal = controller.listaOriginal.lista;
             var id = itemLista.id;
-
             for (var i = 0; i < listaOriginal.length; i++) {
                 if (listaOriginal[i]['id'] == id){
                     itemLista.minhaDescricao = listaOriginal[i].descricaoOriginal;
+                    break;
                 }
             }
 
             itemLista.exibir = false;
+
+            controller.habilitarSalvar();
+        }
+
+        this.confirmarEdicao = function (itemLista) {
+
+            debugger;
+            itemLista.descricaoOriginal = itemLista.minhaDescricao;
+
+            itemLista.exibir = false;
+
+            controller.habilitarSalvar();
+        }
+
+        this.cancelarEdicao = function (itemLista) {
+            
+            itemLista.minhaDescricao = itemLista.descricaoOriginal;
+
+            itemLista.exibir = false;
+
+            controller.habilitarSalvar();
+        }
+
+        this.habilitarSalvar = function(){
+            var listaParaEdicao = controller.produtos.lista;
+            var listaOriginal = controller.listaOriginal.lista;
+
+            controller.exibirBotoes = false;
+            debugger;
+
+            for (i = 0; i < listaParaEdicao.length; i++){
+                var edicao = listaParaEdicao[i];
+                for (j = 0; j < listaOriginal.length; j++) {
+                    var original = listaOriginal[j];
+                    if (edicao['id'] === original['id'] && (edicao['preco'] != original['preco'] || edicao['minhaDescricao'] || original['minhaDescricao'])){
+                        controller.exibirBotoes = true;
+                        return;
+                    }
+                }
+            }
         }
     });
 
